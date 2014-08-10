b052b6609c47a2501f2e4801e5aff26a8351d35c
Luke <LukeLeber@gmail.com>
2014-08-10 12:18:19 -0400

    Docs!

--- app/src/main/java/com/lukeleber/barcodestudio/Module.java
+++ app/src/main/java/com/lukeleber/barcodestudio/Module.java
@@ -8,23 +8,46 @@
 
 package com.lukeleber.barcodestudio;
 
-// todo: continue docs...
+/**
+ * Modules are the individual bars or spaces that make up the smallest data units of a barcode.
+ */
 public final class Module
 {
+    /// Is this a bar or a space?
     private final boolean on;
+
+    /// The weight (in units of 'x') of this module
     private final int weight;
 
+    /**
+     * Constructs a Module with the provided 'on' flag and weight
+     *
+     * @param on     is this Module a bar or a space?
+     * @param weight the weight (in units of 'x') of this Module
+     */
     public Module(boolean on, int weight)
     {
         this.on = on;
         this.weight = weight;
     }
 
+    /**
+     * Does this Module represent a bar or a space?
+     *
+     * @return true if this Module represents a bar, otherwise false
+     *
+     */
     public boolean isOn()
     {
         return on;
     }
 
+    /**
+     * Retrieves the weight (in units of 'x') of this Module
+     *
+     * @return the weight of this Module
+     *
+     */
     public int getWeight()
     {
         return weight;
--- app/src/main/java/com/lukeleber/barcodestudio/Sequence.java
+++ app/src/main/java/com/lukeleber/barcodestudio/Sequence.java
@@ -15,29 +15,34 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-
+/**
+ * A Sequence is a series of Modules that together form a barcode encoding
+ */
 public final class Sequence
         implements Iterable<Module>
 {
+    /// The underlying container
     private final List<Module> modules;
 
-    public Sequence(Sequence rhs)
-    {
-        this.modules = new ArrayList<Module>();
-        this.modules.addAll(rhs.modules);
-    }
-
+    /**
+     * Default constructor (the resulting Sequence is empty)
+     */
     public Sequence()
     {
         this.modules = new ArrayList<Module>();
     }
 
-    @SafeVarargs
-    public Sequence(Module... modules)
-    {
-        this.modules = new ArrayList<Module>(Arrays.asList(modules));
-    }
-
+    /**
+     * Internal Constructor - Optionally makes a read-only Sequence.  Any attempts to modify the
+     * resulting Sequence will result in an {@link java.lang.UnsupportedOperationException}
+     *
+     * @param readOnly Should this Sequence be read-only?
+     *
+     * @param firstOn Should the first Module be a bar or a space?
+     *
+     * @param weights The weights of the subsequent Modules
+     *
+     */
     @SafeVarargs
     Sequence(boolean readOnly, boolean firstOn, int... weights)
     {
@@ -50,6 +55,35 @@ public final class Sequence
         }
     }
 
+    /**
+     * Copy constructor
+     *
+     * @param rhs the Sequence to copy into this one
+     *
+     */
+    public Sequence(Sequence rhs)
+    {
+        this.modules = new ArrayList<Module>();
+        this.modules.addAll(rhs.modules);
+    }
+
+    /**
+     * Constructs a Sequence from the provided modules
+     *
+     * @param modules the Modules to populate this Sequence with
+     */
+    @SafeVarargs
+    public Sequence(Module... modules)
+    {
+        this.modules = new ArrayList<Module>(Arrays.asList(modules));
+    }
+
+    /**
+     * Constructs a Sequence with the provided 'on' flag and weights
+     *
+     * @param firstOn Should the first Module be a bar or a space?
+     * @param weights The weights of the subsequent Modules
+     */
     @SafeVarargs
     public Sequence(boolean firstOn, int... weights)
     {
@@ -61,6 +95,12 @@ public final class Sequence
         }
     }
 
+    /**
+     * Constructs a Sequence with the provided weights - with a bar as the first Module
+     *
+     * @param weights The weights of the subsequent Modules
+     *
+     */
     @SafeVarargs
     public Sequence(int... weights)
     {
@@ -73,45 +113,83 @@ public final class Sequence
         }
     }
 
+    /**
+     * Constructs a Sequence from the provided Collection of Modules
+     *
+     * @param modules The collection of Modules to use
+     *
+     */
     Sequence(Collection<Module> modules)
     {
         this.modules = new ArrayList<Module>(modules);
     }
 
+    /**
+     * Appends the provided Sequence onto this Sequence
+     *
+     * @param sequence the Sequence to append
+     *
+     * @return this
+     *
+     */
     public Sequence append(Sequence sequence)
     {
         this.modules.addAll(sequence.modules);
         return this;
     }
 
+    /**
+     * Appends the provided Module onto this Sequence
+     *
+     * @param module the Module to append
+     *
+     * @return this
+     *
+     */
     public Sequence append(Module module)
     {
         this.modules.add(module);
         return this;
     }
 
+    /**
+     * Retrieves a "sub-sequence" from the provided range
+     *
+     * @param begin the start position
+     *
+     * @param end the end position
+     *
+     * @return the requested sub-sequence
+     *
+     * @throws java.lang.IndexOutOfBoundsException if either extent lies outside of the bounds of
+     * this Sequence
+     *
+     */
     public Sequence slice(int begin, int end)
     {
         return new Sequence(this.modules.subList(begin, end));
     }
 
+    /**
+     * Retrieves the number of Modules in this Sequence
+     *
+     * @return the number of Modules in this Sequence
+     *
+     */
     public int size()
     {
         return this.modules.size();
     }
 
-    public void add(Sequence sequence)
-    {
-        for (Module m : sequence)
-        {
-            this.modules.add(m);
-        }
-    }
-
+    /**
+     * Retrieves an Iterator to the underlying container
+     *
+     * @return an Iterator to the underlying container
+     *
+     */
     @Override
     public Iterator<Module> iterator()
     {
         return modules.iterator();
     }
-
 }
